50dc4f829ee7a70e94aa5690b59b2d652eaa7238
TheRolf <yann.lv@live.fr>
2021-02-27 10:38:35 +0100

    Limited to one instance

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const { app } = require('electron')
 const Store = require('electron-store')
 const autoLaunch = require('./src/autolaunch')
 const monitorChange = require('./src/monitor-change')
-const tray = require('./src/tray')
+const trayModule = require('./src/tray')
 const twitch = require('./src/twitch-on-top')
 const colorpicker = require('./src/colorpicker')
 const mss = require('./src/minecraft-server-status')
 // const pomodoro = require('./src/pomodoro-timer')
 const express = require('./src/deezer-rpc/server')
 
+// limit to single instance because big errors else
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    // Someone tried to run a second instance, open the tray menu
+    if (trayModule.isReady()) {
+      trayModule._tray.popUpContextMenu()
+    }
+  })
+}
+
 autoLaunch.start('Therolf Utility') // set autolaunch name
+
 const store = new Store() // init store
 monitorChange.setStore(store) // setting store for monitor change
 twitch.init(store) // init event handling for twitch
@@ -20,14 +34,14 @@ mss.setServerIP('minecraftutbm.fr:10154') // setting server url
 app.on('ready', () => {
   monitorChange.onReady() // init store for monitor change
 
-  tray.items.push(autoLaunch.trayItem) // adds the auto launch checkbox option
-  tray.items.push(monitorChange.trayItem) // adds the monitor item
-  tray.items.push(twitch.trayItem) // adds the twitch on top item
-  tray.items.push(colorpicker.trayItem) // adds colorpicker tray item
-  tray.items.push(mss.muteTrayItem) // adds minecraft server status checkbox mute item
-  tray.items.push(mss.trayItem) // adds minecraft server status item
+  trayModule.items.push(autoLaunch.trayItem) // adds the auto launch checkbox option
+  trayModule.items.push(monitorChange.trayItem) // adds the monitor item
+  trayModule.items.push(twitch.trayItem) // adds the twitch on top item
+  trayModule.items.push(colorpicker.trayItem) // adds colorpicker tray item
+  trayModule.items.push(mss.muteTrayItem) // adds minecraft server status checkbox mute item
+  trayModule.items.push(mss.trayItem) // adds minecraft server status item
   // tray.items.push(pomodoro.trayItem) // adds pomodoro timer item
-  tray.onReady() // starts the tray
+  trayModule.onReady() // starts the tray
 
   mss.listen() // VERY IMPORTANT : listens to the api
 })
--- src/tray.js
+++ src/tray.js
@@ -1,8 +1,10 @@
 const { Menu, Tray, app } = require('electron')
+const { join } = require('path')
 
-let _tray = undefined
+let _tray
+let _isReady = false
 // define the items and the quit item of tray
-let items = [
+const items = [
   {
     label: 'Quit',
     click: () => {
@@ -15,11 +17,17 @@ let items = [
 // on ready function
 const onReady = () => {
   // create the tray
-  _tray = new Tray(__dirname + '/img/icon.ico')
+  _tray = new Tray(join(__dirname, '/img/icon.ico'))
+
   // set its tooltip name
   _tray.setToolTip('TheRolf Utility')
+
   // build and set its context menu
   _tray.setContextMenu(Menu.buildFromTemplate(items))
+
+  // set ready
+  _isReady = true
+
   // open tray on left click too (Win-Mac only)
   _tray.on('click', () => {
     _tray.popUpContextMenu()
@@ -27,18 +35,19 @@ const onReady = () => {
 }
 
 const check = (label, checked) => {
-  let foundIndex = items.findIndex(item => 'type' in item && item.type == 'checkbox' && item.label == label)
+  const foundIndex = items.findIndex(item => 'type' in item && item.type === 'checkbox' && item.label === label)
 
-  if (foundIndex != -1) items[foundIndex].checked = checked
+  if (foundIndex !== -1) items[foundIndex].checked = checked
 
   // rebuild the context menu
   _tray.setContextMenu(Menu.buildFromTemplate(items))
 }
 
-//export
+// export
 module.exports = {
   _tray: _tray,
   items: items,
   onReady: onReady,
-  setChecked: check
+  setChecked: check,
+  isReady: () => { return _isReady }
 }
